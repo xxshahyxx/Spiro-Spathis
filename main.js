33483a4d2faa0d6f753fd7bf391dc5ae45cf81d9
xxshahyxx <155009625+xxshahyxx@users.noreply.github.com>
2023-12-27 19:55:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+let text=document.querySelector('.text')
+function spero(spero){
+    img.src = spero;}
+
+    function colors(color){
+        container .style .background= color;  
+    }
+    
+ 
+    
